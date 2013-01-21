eeeccee4bb5105787716c9eaf0ebe1d9558026a2
Uwe Kleinmann <u.kleinmann@googlemail.com>
2013-01-21 20:53:44 +0100

    Remove some packages because they will get installed by puppet-gitlab-redmine-common.

--- manifests/pre.pp
+++ manifests/pre.pp
@@ -20,36 +20,8 @@ class redmine::pre {
 class redmine::ubuntu_packages {
   include redmine
 
-  $redmine_dbtype  = $redmine::redmine_dbtype
-
-  exec {
-    'apt-get update':
-      command     => '/usr/bin/apt-get update';
-  }
-
-  $db_packages = $redmine_dbtype ? {
-    mysql => ['libmysql++-dev','libmysqlclient-dev'],
-    pgsql => ['libpq-dev', 'postgresql-client'],
-  }
   package {
-    $db_packages:
-      ensure  => installed,
-      require => Exec['apt-get update']
-  }
-
-  package {
-    ['libmagickcore-dev','libmagickwand-dev',
-      'libxml2-dev','libxslt1-dev']:
+    ['libmagickcore-dev','libmagickwand-dev']:
         ensure  => installed,
-        require => Exec['apt-get update'],
   }
-
-  # Assuming default ruby 1.9.3 (quantal)
-  package {
-    ['ruby','ruby-dev','rubygems']:
-      require => Exec['apt-get update'],
-      ensure  => installed;
-  }
-
 } # Class:: redmine::pre
-
